6ffba864ecc3df8bf908c04ab9cf6604c3d03191
Alexander Raevskiy <aeg2611@gmail.com>
2020-12-28 20:15:40 +0300

    10 задание

--- fixture/contact.py
+++ fixture/contact.py
@@ -48,7 +48,8 @@ class ContactHelper:
     def return_to_homepage(self):
         wd = self.app.wd
         # return to homepage
-        wd.find_element_by_link_text("home page").click()
+        if not (wd.current_url.endswith("/index.php") and len(wd.find_elements_by_name("MainForm")) > 0):
+            wd.find_element_by_link_text("home page").click()
 
     def count(self):
         wd = self.app.wd
