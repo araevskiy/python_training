97e97f3e00f0dd5ed45ee26dbce791d651af1fd7
Alexander Raevskiy <aeg2611@gmail.com>
2020-12-28 16:21:33 +0300

    9 задание

--- fixture/session.py
+++ fixture/session.py
@@ -18,6 +18,11 @@ class SessionHelper:
         wd.find_element_by_link_text("Logout").click()
         wd.find_element_by_name("user")
 
+    def ensure_logout(self):
+        wd = self.app.wd
+        if self.is_logged_in():
+            self.logout()
+
     def is_logged_in(self):
         wd = self.app.wd
         return len(wd.find_elements_by_link_text("Logout")) > 0
@@ -25,11 +30,6 @@ class SessionHelper:
     def is_logged_in_as(self, username):
         wd = self.app.wd
         return wd.find_element_by_xpath("//div[@id='top']/form/b").text == "("+username+")"
-
-    def ensure_logout(self):
-        wd = self.app.wd
-        if self.is_logged_in():
-            self.logout()
 
     def ensure_login(self, username, password):
         wd = self.app.wd
