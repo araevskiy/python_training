3d299232108424b1ff0ceb8ee57ff893359256c6
araevskiy <75529555+araevskiy@users.noreply.github.com>
2020-12-28 16:14:55 +0300

    Add files via upload

---
+++ contact.py
@@ -0,0 +1,62 @@
+class ContactHelper:
+
+    def __init__(self, app):
+        self.app= app
+
+    def create(self, contact):
+        wd = self.app.wd
+        # init new contact
+        wd.find_element_by_link_text("add new").click()
+        # fill contact form
+        self.fill_contact_form(contact)
+        # submit contact creation
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+        self.return_to_homepage()
+
+    def fill_contact_form(self, contact):
+        wd = self.app.wd
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.secondname)
+
+    def logout(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("Logout").click()
+        wd.find_element_by_name("user")
+
+    def modify_contact(self,contact):
+        wd = self.app.wd
+        wd.find_element_by_xpath("//img[@alt='Edit']").click()
+        self.fill_contact_form(contact)
+        wd.find_element_by_name("update").click()
+        wd.find_element_by_link_text("home").click()
+
+
+    def delete_first_contact(self):
+        wd = self.app.wd
+        # select contact
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_xpath("//input[@value='Delete']").click()
+        wd.switch_to_alert().accept()
+        wd.find_element_by_link_text("home").click()
+
+
+    def return_to_homepage(self):
+        wd = self.app.wd
+        # return to homepage
+        wd.find_element_by_link_text("home page").click()
+
+    def count(self):
+        wd = self.app.wd
+        self.app.open_home_page()
+        return len(wd.find_elements_by_name("selected[]"))
+
+
+
+
+
+
