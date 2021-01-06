5b90c977ade9b322edeebd9bffd08356147a0dbe
Alexander Raevskiy <aeg2611@gmail.com>
2021-01-06 15:42:53 +0300

    11 задание исправлено

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -4,9 +4,9 @@ from model.contact import Contact
 
 def test_modify_contact(app):
         if app.contact.count() == 0:
-                app.contact.create(Contact(firstname="11", secondname="22"))
+                app.contact.create(Contact(firstname="11", secondname="22",lastname="gg"))
         old_contacts = app.contact.get_contact_list()
-        contact = Contact(firstname="Aleуss56", secondname="RYsss")
+        contact = Contact(firstname="Aleуss56", secondname="RYsss",lastname="gg1")
         contact.id = old_contacts[0].id
         app.contact.modify_contact(contact)
         new_contacts = app.contact.get_contact_list()
