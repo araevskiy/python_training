4043beac61e10afc7969a0d51ed720815bdd00c1
Alexander Raevskiy <aeg2611@gmail.com>
2021-01-31 00:54:35 +0300

    22 исправление

--- fixture/orm.py
+++ fixture/orm.py
@@ -7,7 +7,7 @@ class ORMFixture:
     db = Database()
     class ORMGroup(db.Entity):
         _table_ = 'group_list'
-        identifier = PrimaryKey(int, column='group_id')
+        id = PrimaryKey(int, column='group_id')
         name = Optional(str, column='group_name')
         header = Optional(str, column='group_header')
         footer = Optional(str, column='group_footer')
@@ -15,7 +15,7 @@ class ORMFixture:
 
     class ORMContact(db.Entity):
         _table_ = 'addressbook'
-        identifier = PrimaryKey(int, column='id')
+        id = PrimaryKey(int, column='id')
         firstname = Optional(str, column='firstname')
         lastname = Optional(str, column='lastname')
         deprecated = Optional(str, column='deprecated')
@@ -28,7 +28,7 @@ class ORMFixture:
 
     def convert_groups_to_model(self, groups):
         def convert(group):
-            return Group(id=str(group.identifier), name=group.name, header=group.header, footer=group.footer)
+            return Group(id=str(group.id), name=group.name, header=group.header, footer=group.footer)
         return list(map(convert, groups))
 
     @db_session
@@ -37,7 +37,7 @@ class ORMFixture:
 
     def convert_contacts_to_model(self, contacts):
         def convert(contact):
-            return Contact(id=str(contact.identifier), firstname=contact.firstname, lastname=contact.lastname)
+            return Contact(id=str(contact.id), firstname=contact.firstname, lastname=contact.lastname)
         return list(map(convert, contacts))
 
     @db_session
@@ -46,12 +46,12 @@ class ORMFixture:
 
     @db_session
     def contacts_in_group(self, group):
-        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.identifier == group.identifier))[0]
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
         return self.convert_contacts_to_model(orm_group.contacts)
 
     @db_session
     def contacts_not_in_group(self, group):
-        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.identifier == group.identifier))[0]
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
         return self.convert_contacts_to_model(
             select(c for c in ORMFixture.ORMContact if c.deprecated is None and orm_group not in c.groups))
 
